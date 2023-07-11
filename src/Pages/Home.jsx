34c42dbac7dd2a2226ddd28025a4fafaa8c48661
Samy Reda <s.reda@intrazero.net>
2023-07-11 10:12:42 +0300

    initial commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../assets/Components/Header'
+import Search from '../assets/Components/Search'
+import FeaturedPosts from '../assets/Components/FeaturedPosts'
+import Footer from '../assets/Components/Footer'
+
+function Home() {
+  return (
+    <>
+    <Header />
+    <Search />
+    <FeaturedPosts />
+    <Footer />
+    </>
+  )
+}
+
+export default Home
